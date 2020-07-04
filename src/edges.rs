834e1dae4d70c0099699d84115ed7a13c3c835a4
Christian <christian@chfi.se>
2020-07-04 11:41:14 -0500

    Update for latest handlegraph

--- src/edges.rs
+++ src/edges.rs
@@ -1,13 +1,13 @@
 use gfa::gfa::GFA;
 
 use handlegraph::handle::Direction;
-use handlegraph::handlegraph::{handle_iter, HandleGraph};
+use handlegraph::handlegraph::{handles_iter, HandleGraph};
 
 pub fn graph_edge_count<T: HandleGraph>(graph: &T) -> Vec<(u64, usize, usize, usize)> {
-    handle_iter(graph)
+    handles_iter(graph)
         .map(|h| {
-            let inbound = graph.get_degree(&h, Direction::Left);
-            let outbound = graph.get_degree(&h, Direction::Right);
+            let inbound = graph.degree(h, Direction::Left);
+            let outbound = graph.degree(h, Direction::Right);
             let total = inbound + outbound;
             let id: u64 = h.unpack_number();
 
